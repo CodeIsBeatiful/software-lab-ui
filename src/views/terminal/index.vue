f53e26f96a47334e05e38f62323864d72d59898f
blackstar-baba <535650957@qq.com>
2020-06-16 23:27:48 +0800

    add enter and backspace event

--- src/views/terminal/index.vue
+++ src/views/terminal/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div ref="terminal" id="terminal"></div>
+    <div id="terminal" ref="terminal"/>
   </div>
 </template>
 
@@ -8,7 +8,7 @@
 
 import { Terminal } from 'xterm'
 import { FitAddon } from 'xterm-addon-fit'
-import { AttachAddon } from 'xterm-addon-attach'
+// import { AttachAddon } from 'xterm-addon-attach'
 import { WebLinksAddon } from 'xterm-addon-web-links'
 // 这个特别重要,一定要在开头就引入css,否则可能会导致样式错误
 import 'xterm/css/xterm.css'
@@ -19,22 +19,6 @@ export default {
     return {
       socket: '',
       term: null
-    }
-  },
-  methods: {
-    // 节流,避免拖动时候频繁向后端请求更新
-    debounce(fn, wait) {
-      let timeout = null
-      return function() {
-        if (timeout !== null) clearTimeout(timeout)
-        timeout = setTimeout(fn, wait)
-      }
-    },
-    // 页面重新resize的时候,需要重新告诉后端cols和rows
-    resizeScreen() {
-      // 对xterm的窗口重新fit,获取新的cols和rows
-      this.fitAddon.fit()
-      this.socket.send(JSON.stringify([this.term.cols, this.term.rows]))
     }
   },
   mounted() {
@@ -52,7 +36,6 @@ export default {
 
     // 在绑定的组件上初始化窗口
     this.term.open(this.$refs.terminal)
-    this.term.write('Welcome to SoftwareLab $ ')
 
     // 窗口初始化后,按照浏览器窗口自适应大小
     this.fitAddon.fit()
@@ -62,14 +45,62 @@ export default {
 
     // 创建ws实例
     // 这里还把窗口的column和row传入后端,使其能自动针对前端窗口边框改为输出
-    this.socket = new WebSocket(`ws://127.0.0.1:8080/api/ws/test?cols=${this.term.cols}&rows=${this.term.rows}'`)
-
+    // this.socket = new WebSocket(`ws://127.0.0.1:8080/api/ws/test?cols=${this.term.cols}&rows=${this.term.rows}'`)
     // xterm的socket组件与websocket实例结合
-    const attachAddon = new AttachAddon(this.socket)
-    this.term.loadAddon(attachAddon)
+    // const attachAddon = new AttachAddon(this.socket)
+    // this.term.loadAddon(attachAddon)
+
+    // 参考官方demo
+    // https://github.com/xtermjs/xterm.js/blob/master/demo/client.ts
+    this.term.prompt = () => {
+      this.term.write('\r\n$ ')
+    }
+
+    this.term.writeln('Welcome to Software Lab')
+    this.term.writeln('Software Lab allows you to quickly get an application with one click')
+    this.term.writeln('')
+    this.term.prompt()
+
+    this.term.onKey((e) => {
+      const ev = e.domEvent
+      const printable = !ev.altKey && !ev.ctrlKey && !ev.metaKey
+
+      if (ev.keyCode === 13) { // Enter
+        this.term.prompt()
+        // todo do some
+      } else if (ev.keyCode === 8) { // Backspace
+        // Do not delete the prompt
+        if (this.term._core.buffer.x > 2) {
+          this.term.write('\b \b')
+        }
+      } else if (printable) {
+        this.term.write(e.key)
+      }
+    })
+
+    this.term.onLineFeed((obj) => {
+      console.info(obj)
+    })
 
     // 监听resize,当窗口拖动的时候,监听事件,实时改变xterm窗口
     window.addEventListener('resize', this.debounce(this.resizeScreen, 1500), false)
+  },
+  methods: {
+    // 节流,避免拖动时候频繁向后端请求更新
+    debounce(fn, wait) {
+      let timeout = null
+      return function() {
+        if (timeout !== null) clearTimeout(timeout)
+        timeout = setTimeout(fn, wait)
+      }
+    },
+    // 页面重新resize的时候,需要重新告诉后端cols和rows
+    resizeScreen() {
+      // 对xterm的窗口重新fit,获取新的cols和rows
+      this.fitAddon.fit()
+      this.socket.send(JSON.stringify([this.term.cols, this.term.rows]))
+    }
   }
+
 }
 </script>
